5e7eea8fedf671af2b194036d2d8b132995b00af
Lucy Yuewei Yuan <32430644+LucyYYW@users.noreply.github.com>
2020-04-10 20:10:52 -0400

    Group gsr others (#331)
    * moving add group button to top
    
    * add group button fixed
    
    * adjust text
    
    * admin indicator on group manage page
    
    * making admin indicator larger
    
    * alert for success/error group booking
    
    * fixing colors for status icon on confirm booking page
    
    * move admin label to the right
    
    * show error message & show success iff completeSuccess

--- PennMobile/GSR-Booking/Views/GSRGroups/CreateGroupCell.swift
+++ PennMobile/GSR-Booking/Views/GSRGroups/CreateGroupCell.swift
@@ -14,17 +14,33 @@ class CreateGroupCell: UITableViewCell {
     static let identifier = "createGroupCell"
     
     fileprivate var titleLabel: UILabel!
+    fileprivate var addButton:UIButton!
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        let bgView = UIView()
+        bgView.layer.cornerRadius = 8
+        bgView.backgroundColor = UIColor(named:"baseLabsBlue")
+
+
+        addSubview(bgView)
+        bgView.translatesAutoresizingMaskIntoConstraints = false
+        _ = bgView.anchor(topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 8, leftConstant: 15, bottomConstant: 8, rightConstant: 15)
+
         titleLabel = UILabel()
-        titleLabel.text = "Add Group"
+        titleLabel.text = "Create a New Group"
         titleLabel.font = UIFont.systemFont(ofSize: 17, weight: .semibold)
-        titleLabel.textColor = UIColor(named: "baseLabsBlue")
-        addSubview(titleLabel)
+        titleLabel.textColor = UIColor.white
+        bgView.addSubview(titleLabel)
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         titleLabel.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
         titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+
+        
+        
+        
+        
     }
     
     required init?(coder aDecoder: NSCoder) {
